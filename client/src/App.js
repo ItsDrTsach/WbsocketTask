08fe82c7af292ed57a030a6c21c380725e8be34e
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2022-05-03 10:43:56 +0300

    deleted server
    decided to manage it only on the front side with simple react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [bitcoinPrice, setbitcoinPrice] = useState(null);
+  const [ethereumPrice, setethereumPrice] = useState(null);
+  useEffect(() => {
+    const ws = new WebSocket(
+      "wss://stream.binance.com:9443/stream?streams=btcusdt@ticker/ethusdt@ticker"
+    );
+    ws.onmessage = (event) => {
+      let { data: stockObject } = JSON.parse(event.data);
+      let { s: symbol, c: lastPrice } = stockObject;
+      console.log(symbol, lastPrice);
+      symbol === "BTCUSDT"
+        ? setbitcoinPrice(lastPrice)
+        : setethereumPrice(lastPrice);
+    };
+  }, []);
+  return (
+    <div className="App">
+      {bitcoinPrice && <h2>Bitcoin Price is {bitcoinPrice} $</h2>}
+      {ethereumPrice && <h2>Ethereum Price is {ethereumPrice} $</h2>}
+    </div>
+  );
 }
 
 export default App;
